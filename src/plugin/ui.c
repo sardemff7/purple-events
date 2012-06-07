9496a8d94fd1a3ea2acf90f4d6f6858ac021fc5d
Quentin Glidic <sardemff7+git@sardemff7.net>
2012-06-07 20:06:48 +0200

    ui: Add stack-events preference

--- src/plugin/ui.c
+++ src/plugin/ui.c
@@ -118,6 +118,12 @@ purple_events_ui_get_pref_frame(PurplePlugin *plugin)
     );
     purple_plugin_pref_frame_add(frame, pref);
 
+    pref = purple_plugin_pref_new_with_name_and_label(
+        "/plugins/core/libnotify+/stack-events",
+        _("Stack events (do not ignore a buddy action if an event is already attached to)")
+    );
+    purple_plugin_pref_frame_add(frame, pref);
+
     return frame;
 }
 
